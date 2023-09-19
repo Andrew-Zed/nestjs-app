6dab6985ae0a2d10999109fe51cd6f0edfdf0629
Andrew-Zed <andrew.awadyke@gmail.com>
2023-09-19 01:01:17 +0100

    find a user and find all users impl in controller

--- auto-shop/src/users/users.controller.ts
+++ auto-shop/src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body,Controller, Post } from '@nestjs/common';
+import { Body,Controller, Post, Get, Param, Query } from '@nestjs/common';
 import { createUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 
@@ -12,6 +12,16 @@ export class UsersController {
     @Post('/signup') 
     createUser (@Body() body: createUserDto) {
         this.userService.create(body.email, body.password);
+    }
+
+    @Get('/:id')
+    findUser(@Param('id') id: string) {
+        return this.userService.findOne(parseInt(id));
+    }
+
+    @Get()
+    findAllUsers(@Query('email') email: string) {
+        return this.userService.find(email);
     }
 
 }
